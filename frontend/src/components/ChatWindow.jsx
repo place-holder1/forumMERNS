5f0a591dd1afe542723aa131b29f70279c798e67
place-holder1 <138268286+place-holder1@users.noreply.github.com>
2025-04-08 16:06:15 -0400

    chatwindow
    chatwindow basic like reddit

---
+++ frontend/src/components/ChatWindow.jsx
@@ -0,0 +1,16 @@
+import styles from "../styles/chat-window.module.css"
+
+const ChatWindow = () => {
+  return (
+    <div className={styles.chatContainer}>
+        <div className={styles.chatHeader}>
+
+        </div>
+        <div className="chatBoxTextContainer">
+
+        </div>
+    </div>
+  )
+}
+
+export default ChatWindow
