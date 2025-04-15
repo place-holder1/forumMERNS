282942e5537a41c59e4c46c011a41ce1420c2753
place-holder1 <138268286+place-holder1@users.noreply.github.com>
2025-04-15 16:42:21 -0400

    topic threads
    translating from php to react

--- frontend/src/components/forumComponents/ForumThread.jsx
+++ frontend/src/components/forumComponents/ForumThread.jsx
@@ -1,10 +1,57 @@
 import styles from "../../styles/forum-thread.module.css"
+import {useState, useEffect} from "react"
 
-const forumThread = () => {
+const ForumThread = () => {
  
+    const [categoryThread, setCategoryThread] = useState({
+        topic: "Welcome!",
+        description: "hi"
+    });
+
+    const [topicThread, setTopicThread] = useState({
+        image: "Image",
+        topic: "Yes",
+        description: "Hi!"
+    });
+
     return (
-        0
+        <div className={styles.category_container}>
+            <h2 className={styles.category_header}>
+                {categoryThread.topic}
+                <span className={styles.desc}>{categoryThread.description}</span>
+            </h2>
+            <div className={styles.topic_container}>
+                <span className={styles.topic_image}>{topicThread.image}</span>
+                <div className={styles.topic_text_container}>
+                    {topicThread.topic}
+                    {/* <span className={styles.desc}>{topicThread.description}</span> */}
+
+                </div>
+                <div className={styles.topic_stats}>
+
+                </div>
+                <div className={styles.topic_recent_post}>
+                    
+                </div>
+            </div>
+            <div className={styles.topic_container}>
+                <span className={styles.topic_image}>{topicThread.image}</span>
+                <div className={styles.topic_text_container}>
+                    {topicThread.topic}
+                    {/* <span className={styles.desc}>{topicThread.description}</span> */}
+                    
+                </div>
+            </div>
+            <div className={styles.topic_container}>
+            <span className={styles.topic_image}>{topicThread.image}</span>
+                <div className={styles.topic_text_container}>
+                    {topicThread.topic}
+                    {/* <span className={styles.desc}>{topicThread.description}</span> */}
+                    
+                </div>
+            </div>
+        </div>
     );
 };
 
-export default forumThread;
+export default ForumThread;
--- frontend/src/components/forumComponents/FrontMain.jsx
+++ frontend/src/components/forumComponents/FrontMain.jsx
@@ -1,12 +1,26 @@
 import styles from "../../styles/front.module.css"
+import { useState, useEffect } from "react";
+import ForumThread from "./ForumThread";
 
 const FrontMain = () => {
+
+    const topic = []
+    const ObjectRow = 0;
+    for (let i = 0; i < topic; i++) {
+        ObjectRow()
+    } 
 
     return (
         <div className={styles.frontMain}>
             <div className={styles.topicThreadGroup}>
                 {/* div.topicThreadName */}
+                <ForumThread/>
             </div>
+            <div className={styles.topicThreadGroup}>
+                {/* div.topicThreadName */}
+                <ForumThread/>
+            </div>
+            
         </div>
     )
 
